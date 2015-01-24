75823061820bfded02952fb1a0ece95dea955004
Bartek Kruszczynski <bartek@codequest.com>
2015-01-24 01:12:37 +0100

    First working email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,5 @@ Rails.application.configure do
   config.assets.digest = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 end
