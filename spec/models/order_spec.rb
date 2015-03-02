2279a9f98517f75cac16d4e942c7abe86febd7c9
Bartek Kruszczynski <bartek@codequest.com>
2015-03-02 22:16:09 +0100

    Update gems

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -20,7 +20,7 @@ describe Order, :type => :model do
     end
   end
 
-  describe '#amout' do
+  describe '#amount' do
     it 'should return 0 when no dishes' do
       order = Order.new date: Date.today
       expect(order).to receive(:dishes).and_return([])
