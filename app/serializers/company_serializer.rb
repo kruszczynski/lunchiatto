ab1f34c1c0e886da13b93b5ea8d30638cabdbde7
Bartek Kruszczynski <bartek@codequest.com>
2015-05-27 01:14:14 +0200

    Sort company users by name

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -2,4 +2,8 @@ class CompanySerializer < ActiveModel::Serializer
   attributes :name
   has_many :users
   has_many :invitations
+
+  def users
+    object.users.by_name
+  end
 end
---
+++ spec/serializers/company_serializer_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe CompanySerializer do
+  let(:company) { double("Company") }
+  let(:users) { double("Users") }
+  subject { CompanySerializer.new company }
+
+  describe "#users" do
+    it "sorts'em" do
+      expect(users).to receive(:by_name) { :sorted_users }
+      expect(company).to receive(:users) { users }
+      expect(subject.users).to eq(:sorted_users)
+    end
+  end
+end
