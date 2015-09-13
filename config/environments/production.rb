1a54d3f0d3a804cd2fc5c6a59a55f27829d627b6
Bartek Kruszczynski <bartek@codequest.com>
2015-09-13 22:33:30 +0200

    bring force_ssl back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -82,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.force_ssl = true
+  config.force_ssl = true
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
