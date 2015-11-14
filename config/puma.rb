680d04cd66e0522f9aeeaabc1e70cbacd591f9f8
Bartek Kruszczynski <kruszczyk@gmail.com>
2015-11-14 20:53:41 +0100

    Optimize puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 3)
-threads_count = Integer(ENV['MAX_THREADS'] || 1)
-threads threads_count, threads_count
+max_threads_count = Integer(ENV['MAX_THREADS'] || 1)
+min_threads_count = Integer(ENV['MIN_THREADS'] || 1)
+threads min_threads_count, max_threads_count
 
 preload_app!
 
-rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
@@ -12,4 +12,8 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
 end
