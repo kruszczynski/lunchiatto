6c84d33576208f913f618ba30d7f736e9c86d4c4
Bartek Kruszczynski <kruszczyk@gmail.com>
2016-02-24 21:54:25 +0100

    Remove google keys from repo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, '236411385092-ntkf5gal79l0puioplgir0sd522ag9lg.apps.googleusercontent.com', 'c5Z1ETLPid3UtjjSA6SEo5--'
+  config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
