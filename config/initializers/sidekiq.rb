97952ede9426d44ccc39949996761480044ba441
Bartek Kruszczynski <kruszczyk@gmail.com>
2016-07-14 22:29:21 +0200

    Add logging to papertrail (#59)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,3 +8,17 @@ end
 Sidekiq.configure_client do |config|
   config.redis = redis_opts
 end
+
+if Rails.env.production?
+  local_logger = Logger.new(STDOUT)
+  local_logger.formatter = Sidekiq::Logging::Pretty.new
+  remote_logger = RemoteSyslogLogger.new(
+    ENV['PAPERTRAIL_HOST'],
+    ENV['PAPERTRAIL_PORT'].to_i,
+    program: 'lunchiatto/sidekiq',
+    local_hostname: 'lunchiatto-sidekiq',
+  )
+  remote_logger.formatter = Sidekiq::Logging::Pretty.new
+  Sidekiq::Logging.logger = MultiLogger.new([local_logger, remote_logger])
+  Sidekiq::Logging.logger.level = Logger::INFO
+end
