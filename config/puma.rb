dfad8832d3255b463d0e14efadb032012dbb46dc
Bartek Kruszczynski <kruszczyk@gmail.com>
2016-08-08 00:50:49 +0200

    Rubo cop cop cop

--- config/puma.rb
+++ config/puma.rb
@@ -9,8 +9,8 @@ max_threads_count = ENV.fetch('MAX_THREADS') { 1 }.to_i
 min_threads_count = ENV.fetch('MIN_THREADS') { 1 }.to_id
 threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
+# Specifies the `port` that Puma will listen on to receive requests, default
+# is 3000.
 port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
