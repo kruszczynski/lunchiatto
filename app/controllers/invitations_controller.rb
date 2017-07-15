7b50d157ae41b03010504782862d3f7150a7659e
Bartek Kruszczynski <kruszczyk@gmail.com>
2017-07-15 18:13:46 +0200

    Remove sign up from the home page (#116)

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 class InvitationsController < ApplicationController
-  def create
-    invitation = Invitation.new(invitation_params)
-    save_record invitation do |saved_invitation|
-      UserAccessMailer.create_email(saved_invitation.email).deliver_later
-    end
-  end
-
   def show
     redirect_to_today
     @invitation = find_invitation
@@ -15,10 +8,6 @@ class InvitationsController < ApplicationController
   end
 
   private
-
-  def invitation_params
-    params.require(:invitation).permit(:email)
-  end
 
   def find_invitation
     Invitation.where(id: params[:id]).first
--- app/mailers/user_access_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class UserAccessMailer < ApplicationMailer
-  def create_email(email)
-    @email = email
-    admin_emails = User.admin.pluck(:email)
-    return if admin_emails.blank?
-    mail(to: admin_emails, subject: "#{@email} would like to join CQM")
-  end
-end
