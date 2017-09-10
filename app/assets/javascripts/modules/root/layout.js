09a18d9e1cf6960defbc581fc711b5b7af9c5be9
Bartek Kruszczyński <bartek@shedul.com>
2017-09-10 22:59:55 +0200

    Decaffeinate root/layout.coffee

---
+++ app/assets/javascripts/modules/root/layout.js
@@ -0,0 +1,10 @@
+window.Lunchiatto.module('Root', (Root, App, Backbone, Marionette, $, _) =>
+  Root.Layout = Marionette.LayoutView.extend({
+    el: 'body',
+
+    regions: {
+      navbar: '#user-panel',
+      content: '#content'
+    }
+  })
+);
