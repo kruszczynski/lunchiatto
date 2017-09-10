d0876c167f4c45172bb7860090ab00c2ff396d52
Bartek Kruszczyński <bartek@shedul.com>
2017-09-10 23:08:06 +0200

    Decaffeinate transfer/views/layout.coffee

---
+++ app/assets/javascripts/modules/transfer/views/layout.js
@@ -0,0 +1,42 @@
+window.Lunchiatto.module('Transfer', function(Transfer, App, Backbone, Marionette, $, _) {
+  return Transfer.Layout = Marionette.LayoutView.extend({
+    template: 'transfers/layout',
+
+    ui: {
+      receivedTransfers: '.received-transfers',
+      submittedTransfers: '.submitted-transfers'
+    },
+
+    behaviors: {
+      Animateable: {
+        types: ['fadeIn']
+      },
+      Titleable: {}
+    },
+
+    regions: {
+      receivedTransfers: '@ui.receivedTransfers',
+      submittedTransfers: '@ui.submittedTransfers'
+    },
+
+    onRender() {
+      this._showTransfers('received');
+      this._showTransfers('submitted');
+    },
+
+    _showTransfers(type) {
+      const transfers = new App.Entities.Transfers([], {type});
+      transfers.optionedFetch({
+        success: transfers => {
+          const view = new App.Transfer.List({
+            collection: transfers});
+          this[`${type}Transfers`].show(view);
+        }
+      });
+    },
+
+    _htmlTitle() {
+      return 'Transfers';
+    }
+  });
+});
