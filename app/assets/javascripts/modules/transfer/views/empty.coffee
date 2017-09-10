4b351e6c19cb598dabb90ad86f7768d37e66be9b
Bartek Kruszczyński <bartek@shedul.com>
2017-09-10 23:05:31 +0200

    Decaffeinate transfer/views/empty.js

--- app/assets/javascripts/modules/transfer/views/empty.coffee
+++
@@ -1,4 +0,0 @@
-@Lunchiatto.module 'Transfer', (Transfer, App, Backbone, Marionette, $, _) ->
-  Transfer.Empty = Marionette.ItemView.extend
-    template: 'transfers/empty'
-    className: 'transfer-box__empty'
---
+++ app/assets/javascripts/modules/transfer/views/empty.js
@@ -0,0 +1,6 @@
+window.Lunchiatto.module('Transfer', (Transfer, App, Backbone, Marionette, $, _) =>
+  Transfer.Empty = Marionette.ItemView.extend({
+    template: 'transfers/empty',
+    className: 'transfer-box__empty'
+  })
+);
