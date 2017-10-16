32f90190e91d2396714b7729748422da95b43ff8
Bartek Kruszczyński <bartek@shedul.com>
2017-10-16 20:11:21 +0200

    Add /users/me endpoint

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,31 +2,29 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
-  let!(:user) { create :user }
+  let!(:user) { create(:user, account_number: '23421234') }
   let!(:other_user) { create :other_user }
   describe 'PUT :update' do
-    describe 'json' do
-      let(:update_params) { {id: user.id, account_number: '13'} }
-      let(:call) { put :update, params: update_params, format: :json }
+    let(:update_params) { {id: user.id, account_number: '13'} }
+    let(:call) { put :update, params: update_params, format: :json }
 
-      context 'when success' do
-        before { sign_in user }
+    context 'when success' do
+      before { sign_in user }
 
-        it 'returns user object on json' do
-          call
-          expect(response).to have_http_status(:success)
-        end
-
-        it 'changes account number' do
-          call
-          expect(user.reload.account_number).to eq('13')
-        end
-      end
-
-      it 'returns 401 for unlogged in json' do
+      it 'returns user object on json' do
         call
-        expect(response).to have_http_status(401)
+        expect(response).to have_http_status(:success)
       end
+
+      it 'changes account number' do
+        call
+        expect(user.reload.account_number).to eq('13')
+      end
+    end
+
+    it 'returns 401 for unlogged in json' do
+      call
+      expect(response).to have_http_status(401)
     end
   end
 
@@ -53,16 +51,43 @@ RSpec.describe Api::UsersController, type: :controller do
   end
 
   describe 'GET :show' do
-    describe 'json' do
-      it 'rejects when not logged in' do
-        get :show, params: {id: user.id}, format: :json
-        expect(response).to have_http_status(401)
-      end
-      it 'renders json' do
-        sign_in user
-        get :show, params: {id: user.id}, format: :json
-        expect(response).to have_http_status(:success)
-      end
+    it 'rejects when not logged in' do
+      get :show, params: {id: user.id}, format: :json
+      expect(response).to have_http_status(401)
+    end
+
+    it 'renders json' do
+      sign_in user
+      get :show, params: {id: user.id}, format: :json
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET :me' do
+    it 'rejects when not logged in' do
+      get :me, format: :json
+      expect(response).to have_http_status(401)
+    end
+
+    it 'returns a success' do
+      sign_in user
+      get :me, format: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns current user' do
+      sign_in user
+      get :me, format: :json
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).to eq(
+        'account_balance' => '0.00',
+        'account_number' => '23421234',
+        'admin' => false,
+        'id' => user.id,
+        'name' => 'Bartek Szef',
+        'pending_transfers_count' => 0,
+        'total_balance' => '0.00',
+      )
     end
   end
 end
